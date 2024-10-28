e0da11acb89362a3bc29120d334e456c59d4d127
marieham45 <marie.hamsikova@centrum.cz>
2024-10-28 20:08:05 +0100

    řešení úlohy elipsa

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Zlovolní zahrádkáři nám chtějí ztížit výměru pozemků a proto si pořídíli pozemek ve tvaru elipsy.
+
+Z matematiky víme, že známe-li šířku a výšku elipsy, její obsah je polovina šířky krát polovina výšky krát číslo π (tj. přibližně 3,14).
+
+Založte si prázdnou stránku s JavaScriptovým souborem a napište funkci ellipseArea, která spočítá (vrátí) plochu pozemku dle zadané šířky a výšky. Číslo π najdete v JavaScriptu v proměnné Math.PI.
+
+Funkci ellipseArea otestujte (např. pomocí document.body.innerHTML += vypište výsledek volání pro různé hodnoty do stránky).
+
+Pro šířku 1 a výšku 2 by mělo vyjít 1.5707963267948966.
+Pro 2 a 2 pak 3.141592653589793.
+*/
+
+const ellipseArea = (height, width) => {
+  return height * 0.5 * width * 0.5 * Math.PI;
+};
+
+document.body.innerHTML += `<p>${ellipseArea(1, 2)}</p>`;
+document.body.innerHTML += `<p>${ellipseArea(2, 2)}</p>`;
